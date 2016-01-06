0053cabd0292403472822bcd359ecd9d568a6fb1
Leon Sackhoff <blackorpheussfdghdfnsd.dferasdf>
2016-01-06 20:21:28 +0100

    w speed

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -15,7 +15,7 @@ public class Camera {
 
 	public void move(){
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-			position.z-=0.2f;
+			position.z-=0.02f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
 			position.x-=0.02f;
